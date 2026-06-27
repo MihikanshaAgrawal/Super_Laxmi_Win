b33c9e6d81cb3775ac50636c36501a130c00d925
Mihikansha <mihikanshaagrawal@gmail.com>
2026-06-27 11:48:33 +0530

    fix today random numbers

--- app.js
+++ app.js
@@ -116,7 +116,11 @@ app.get("/api/results", (req, res) => {
 
 
 
-      const startDate = new Date(now.getFullYear(), 0, 1);
+      const startDate = new Date(
+        now.getFullYear(),
+        now.getMonth(),
+        now.getDate()
+      );
 
       for (
         let d = new Date(startDate);
@@ -185,6 +189,39 @@ app.get("/api/results", (req, res) => {
           );
         }
       });
+
+
+
+      for (let hour = 8; hour < 22; hour++) {
+  for (let min of [0, 20, 40]) {
+
+    const slotTime = new Date(now);
+    slotTime.setHours(hour, min, 0, 0);
+
+    const timeStr = slotTime.toLocaleTimeString("en-US", {
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true
+    }).toLowerCase();
+
+    const key =
+      `${slotTime.getFullYear()}-${String(slotTime.getMonth() + 1).padStart(2, "0")}-${String(slotTime.getDate()).padStart(2, "0")}_${timeStr}`;
+
+    if (slotTime <= now && !results[key]) {
+
+      const random = Math.floor(Math.random() * 100)
+        .toString()
+        .padStart(2, "0");
+
+      results[key] = random;
+
+      db.run(
+        "INSERT OR IGNORE INTO results(slot_key, number, is_locked) VALUES (?, ?, ?)",
+        [key, random, 1]
+      );
+    }
+  }
+}
 
 
 
