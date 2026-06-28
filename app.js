9c54b1ae289c456274feea5569acfb1b614560f7
Mihikansha <mihikanshaagrawal@gmail.com>
2026-06-29 00:04:01 +0530

    Added loader verification file

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const db = require("./db");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static("public"));
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
